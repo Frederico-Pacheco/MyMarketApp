e0d137c8d566f4ec1920f1933be44c374015cc41
Frederico Pacheco <pachecofms@gmail.com>
2014-07-02 17:49:21 -0300

    added gradle

--- src/br/com/mymarket/activities/MainActivity.java
+++ src/br/com/mymarket/activities/MainActivity.java
@@ -205,8 +205,7 @@ public class MainActivity extends AppBaseActivity implements
 						| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
 						| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
 						| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-						| View.SYSTEM_UI_FLAG_FULLSCREEN
-						| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+						| View.SYSTEM_UI_FLAG_FULLSCREEN);
 	}
 
 	public void showNormalUI() {
